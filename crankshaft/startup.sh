746e633f6f221d7082298a55bec07a9dd410955c
rubenverhoef <ruben_verhoef@hotmai.com>
2018-11-14 19:41:33 +0100

    Add rearcam

--- crankshaft/startup.sh
+++ crankshaft/startup.sh
@@ -4,7 +4,20 @@
 # by default this does nothing
 
 # PWM backlight init
-gpio mode 26 pwm
+gpio -g mode 12 pwm
+gpio -g mode 4 in
 
 # LINBus Buttons
 sudo python /boot/crankshaft/custom/linbus.py &
+
+# RearCam
+while true
+do
+gpio -g wfi 4 both
+TRIGGER=`gpio -g read 4`
+if [ $TRIGGER -ne 1 ] ; then
+    (cd /boot/crankshaft/custom/cam_overlay && ./cam_overlay.bin &)
+elif [ $TRIGGER -ne 0 ] ; then
+    killall cam_overlay.bin
+fi
+done
