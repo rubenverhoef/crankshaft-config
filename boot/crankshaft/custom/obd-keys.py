f8c4ab6023ea358687149a81c804ae9e020e053f
rubenverhoef <ruben_verhoef@hotmai.com>
2019-01-15 21:51:25 +0100

    Messive update OBD

--- boot/crankshaft/custom/obd-keys.py
+++ boot/crankshaft/custom/obd-keys.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-import os, sys
+import os, sys, math, time
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "keyboard"))
 sys.path.append(os.path.join(os.path.dirname(__file__), "obd"))
@@ -11,83 +11,142 @@ import keyboard # import for emulating keyboard presses
 import obd #import for reading/sending obd messages
 from obd import OBDCommand
 from obd.protocols import ECU
+from obd.utils import bytes_to_int
+import obd.decoders as d
 
-SW_BASE     = 0x28000000
-SW_NEXT     = 0x00020000
-SW_PREV     = 0x00010000
-SW_VOLUP    = 0x00010000
-SW_VOLDOWN  = 0x00010000
-SW_M        = 0x00010000
-SW_VOICE    = 0x00010000
+# # 223B541 response: 623B5400000000
+# REV_GEAR	= 7 # 623B5400010000
 
-KEY_BASE    = 0x33000000
-KEY_0       = 0x00010000
-KEY_1       = 0x00010000
-KEY_2       = 0x00010000
-KEY_3       = 0x00010000
-KEY_4       = 0x00010000
-KEY_5       = 0x00010000
-KEY_6       = 0x00010000
-KEY_7       = 0x00010000
-KEY_8       = 0x00010000
-KEY_9       = 0x00010000
-KEY_STAR    = 0x00010000
-KEY_HASH    = 0x00010000
-KEY_LL      = 0x00010000
-KEY_LR      = 0x00010000
-KEY_RL      = 0x00010000
-KEY_RR      = 0x00010000
-KEY_TA      = 0x00010000
-KEY_MUSIC   = 0x00010000
-KEY_NEXT    = 0x00010000
-KEY_PREV    = 0x00010000
-KEY_LEFT    = 0x00010000
-KEY_RIGHT   = 0x00010000
-KEY_UP      = 0x00010000
-KEY_DOWN    = 0x00010000
-KEY_OK      = 0x00010000
+# # 2271511 response: 62715100000000
+# LIGHT		= 12 # 62715100080000
+# LIGHT_1		= 11 # 62715100100000
+# LIGHT_2		= 9  # 62715100400000
 
-def sw_clb(data):
-    if(data & SW_PREV):
-        print("SW_PREV")
+# HAND_BRAKE  = 0
 
-def key_clb(data):
-    if(data & KEY_PREV):
-        print("KEY_PREV")
+class Button:
+    def __init__(self, bitSelect, isPressing, button):
+        self.bitSelect  = bitSelect
+        self.isPressing = False
+        self.button     = button
 
+    def press(self):
+        keyboard.press(self.button)
 
-def sw_decode(messages):
-    """ decoder for Radio Key messages """
-    return messages - KEY_BASE
+    def release(self):
+        keyboard.release(self.button)
 
-def key_decode(messages):
-    """ decoder for Steering Wheel messages """
-    return messages - SW_BASE
+# 22833C1 response: 62833C00
+swBase    = 0x62833C00
+# swVolUp   = Button((0x62833C80 ^ swBase), False, "H")
+# swVolDown = Button((0x62833C40 ^ swBase), False, "H")
+swVoice   = Button((0x62833C08 ^ swBase), False, "M")      # Voice Command
+swNext    = Button((0x62833C04 ^ swBase), False, "N")      # Next
+swPrev    = Button((0x62833C02 ^ swBase), False, "V")      # Previous
+swM       = Button((0x62833C01 ^ swBase), False, "return") # Enter
+
+swButtons   = [swVoice, swNext, swPrev, swM]
 
 sw = OBDCommand("Steering Wheel",
                "Decode SW Commands",
-               "ATE1",
+               b"22833C",
                4,
-               sw_decode,
+               d.drop,
                ECU.ALL,
-               False)
+               True,
+               b'0007A5')
 
-key = OBDCommand("Steering Wheel",
-               "Decode SW Commands",
-               "ATE1",
-               4,
-               key_decode,
+def sw_clb(data):
+    if(data.is_null): # Something to test data before using it..
+        return
+    data = bytes_to_int(data.messages[0].data[3:])
+    if(data == 0): # no key pressed, jump out
+        return
+
+    for c, swButton in enumerate(swButtons, 1):
+        if(swButton.bitSelect & data):
+            if(swButton.isPressing is False):
+                swButton.isPressing = True
+                swButton.press()
+        elif(swButton.isPressing is True):
+            swButton.release()
+    return
+
+# 2280511 response: 62805100000000
+keyBase    = 0x62805100000000
+key0       = Button((0x62805100000400 ^ keyBase), False, "0")           # Number 0
+key1       = Button((0x62805100000800 ^ keyBase), False, "1")           # Number 1
+key2       = Button((0x62805100001000 ^ keyBase), False, "2")           # Number 2
+key3       = Button((0x62805100002000 ^ keyBase), False, "3")           # Number 3
+key4       = Button((0x62805100004000 ^ keyBase), False, "4")           # Number 4
+key5       = Button((0x62805100008000 ^ keyBase), False, "5")           # Number 5
+key6       = Button((0x62805100000001 ^ keyBase), False, "6")           # Number 6
+key7       = Button((0x62805100000002 ^ keyBase), False, "7")           # Number 7
+key8       = Button((0x62805100000004 ^ keyBase), False, "8")           # Number 8
+key9       = Button((0x62805100000008 ^ keyBase), False, "9")           # Number 9
+keyStar    = Button((0x628051FFFFFFFF ^ keyBase), False, "P")           # Phone
+keyHash    = Button((0x628051FFFFFFFF ^ keyBase), False, "escape")      # Esc
+keyInfo    = Button((0x628051FFFFFFFF ^ keyBase), False, "left arrow")  # Hamburger Menu
+# keyReject  = Button((0x628051FFFFFFFF ^ keyBase), False, "a")
+keyLL      = Button((0x628051FFFFFFFF ^ keyBase), False, "left arrow")  # Hamburger Menu
+keyLR      = Button((0x628051FFFFFFFF ^ keyBase), False, "B")           # Play/Pause
+keyRL      = Button((0x628051FFFFFFFF ^ keyBase), False, "H")           # Home
+keyRR      = Button((0x628051FFFFFFFF ^ keyBase), False, "M")           # Voice Command
+keyTA      = Button((0x628051FFFFFFFF ^ keyBase), False, "H")           # Home
+keyMusic   = Button((0x628051FFFFFFFF ^ keyBase), False, "H")           # Home
+keyNext    = Button((0x628051FFFFFFFF ^ keyBase), False, "N")           # Next
+keyPrev    = Button((0x628051FFFFFFFF ^ keyBase), False, "V")           # Previous
+# keyLeft    = Button((0x628051FFFFFFFF ^ keyBase), False, "num 1")
+# keyRight   = Button((0x628051FFFFFFFF ^ keyBase), False, "num 2")
+keyUp      = Button((0x628051FFFFFFFF ^ keyBase), False, "num 1")       # Wheel Left
+keyDown    = Button((0x628051FFFFFFFF ^ keyBase), False, "num 2")       # Wheel Right
+keyOK      = Button((0x628051FFFFFFFF ^ keyBase), False, "return")      # Enter
+# keyCD      = Button((0x628051FFFFFFFF ^ keyBase), False, "H")
+# keyRadio   = Button((0x628051FFFFFFFF ^ keyBase), False, "H")
+keyAUX     = Button((0x62805110000000 ^ keyBase), False, "H")           # Home
+# keyPhone   = Button((0x628051FFFFFFFF ^ keyBase), False, "H")
+# keyMenu    = Button((0x628051FFFFFFFF ^ keyBase), False, "H")
+# keyVolUp   = Button((0x628051FFFFFFFF ^ keyBase), False, "H")
+# keyVolDown = Button((0x628051FFFFFFFF ^ keyBase), False, "H")
+
+keyButtons  = [key0, key1, key2, key3, key4, key5, key6, key7, key8, key9,
+               keyStar, keyHash, keyInfo, keyLL, keyLR, keyRL, keyRR, keyTA,
+               keyMusic, keyNext, keyPrev, keyUp, keyDown, keyOK]
+
+key = OBDCommand("Radio Keys",
+               "Decode Radio Keys",
+               b"228051",
+               7,
+               d.drop,
                ECU.ALL,
-               False)
+               True,
+               b'0007A5')
 
-obd.logger.setLevel(obd.logging.DEBUG)
+def key_clb(data):
+    if(data.is_null): # Something to test data before using it..
+        return
+    data = bytes_to_int(data.messages[0].data[3:])
+    if(data == 0): # no key pressed, jump out
+        return
 
-connection = obd.Async()
+    for c, keyButton in enumerate(keyButtons, 1):
+        if(keyButton.bitSelect & data):
+            if(keyButton.isPressing is False):
+                keyButton.isPressing = True
+                keyButton.press()
+        elif(keyButton.isPressing is True):
+            keyButton.release()
+    return
+
+# obd.logger.setLevel(obd.logging.DEBUG)
+
+connection = obd.OBD("COM9", protocol = "B")
 
 connection.supported_commands.add(sw)
-connection.watch(sw, callback=sw_clb)
-
 connection.supported_commands.add(key)
-connection.watch(key, callback=key_clb)
 
-connection.start()
+while(True):
+    sw_clb(connection.query(sw))
+    key_clb(connection.query(key))
+    # time.sleep(0.01)
+    
