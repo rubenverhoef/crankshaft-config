05b9cc6f4532326d6fb4df7858dcae5508a91a0f
rubenverhoef <ruben_verhoef@hotmai.com>
2019-01-16 20:49:06 +0100

    Change startup.sh, remove rearcam trigger and enable obd

--- boot/crankshaft/startup.sh
+++ boot/crankshaft/startup.sh
@@ -5,19 +5,6 @@
 
 # PWM backlight init
 gpio -g mode 12 pwm
-gpio -g mode 4 in
 
 # OBD connection for Steering Wheel and other keys
-# sudo /usr/bin/python3 ./custom/obd-keys.py &
-
-# RearCam
-while true
-do
-gpio -g wfi 4 both
-TRIGGER=`gpio -g read 4`
-if [ $TRIGGER -ne 1 ] ; then
-    (cd /boot/crankshaft/custom/cam_overlay && ./cam_overlay.bin -d /dev/v4l/by-id/usb-fushicai_usbtv007_300000000002-video-index0 &)
-elif [ $TRIGGER -ne 0 ] ; then
-    killall cam_overlay.bin
-fi
-done
+sudo /usr/bin/python3 /boot/crankshaft/custom/obd-keys.py &
